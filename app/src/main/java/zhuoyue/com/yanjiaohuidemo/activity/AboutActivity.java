db713d54ef28706cfb28d6b4adee1062aff32609
奔跑的蘑菇 <282047406@qq.com>
2017-05-12 11:22:10 +0800

    initview

--- app/src/main/java/zhuoyue/com/yanjiaohuidemo/activity/AboutActivity.java
+++ app/src/main/java/zhuoyue/com/yanjiaohuidemo/activity/AboutActivity.java
@@ -9,7 +9,6 @@ import zhuoyue.com.yanjiaohuidemo.R;
  * 这个是关于燕郊汇页面。
  * */
 
-
 public class AboutActivity extends AppCompatActivity {
     private ImageView mAbout_back;
 
@@ -17,7 +16,6 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-//        init
 
 
     }
--- app/src/main/java/zhuoyue/com/yanjiaohuidemo/activity/BootPageActivity.java
+++ app/src/main/java/zhuoyue/com/yanjiaohuidemo/activity/BootPageActivity.java
@@ -1,6 +1,5 @@
 package zhuoyue.com.yanjiaohuidemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import zhuoyue.com.yanjiaohuidemo.R;
